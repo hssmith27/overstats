c34adb0bde409be879c51810b8a84b3acba57994
hssmith27 <hssmith2704@gmail.com>
2024-07-23 14:30:21 -0400

    Win rate added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 function App() {
   const [stats, setStats] = useState({
     endorsementIcon: "https://static.playoverwatch.com/img/pages/career/icons/endorsement/5-8697f241ca.svg#icon",
-    gamesLost: 0,
-    gamesWon: 0,
+    gamesLost: 1,
+    gamesWon: 1,
     icon: "https://d15f34w2p8l1cc.cloudfront.net/overwatch/c3090e3a1dccc58f143ff53801bc0cecb139f0eb1278f157d0b5e29db9104bed.png",
     name: "Player Name"
   })
@@ -30,7 +30,7 @@ function InputID({stats, setStats}) {
               const battleNetID = e.target.value.replace("#", "-");
               fetch('/stats/' + battleNetID).then(res => res.json()).then(data => setStats(data["stats"])).then(
                 document.getElementById("player-summary").style.display = "block"
-              );
+              )
           }}
       }
       />
@@ -40,13 +40,31 @@ function InputID({stats, setStats}) {
 function PlayerSummary({stats}) {
   return (
     <div id = "player-summary">
-      <div id = "player-banner">
+      <TitleCard stats = {stats} />
+      <WinRate stats = {stats} />
+    </div>
+  )
+}
+
+function TitleCard({stats}) {
+  return (
+    <div id = "player-banner">
         <img id = "icon" src = {stats.icon} class></img>
         <h1>{stats.name}</h1>
         <img id = "endorsement" src = {stats.endorsementIcon}></img>
       </div>
+  );
+}
+
+function WinRate({stats}) {
+  return (
+    <div id = "win-rate">
+      <h3>Win Rate </h3>
+      <hr />
+      <div id = "win-rate-graph">{Math.round(stats.gamesWon/(stats.gamesWon + stats.gamesLost) * 10000) / 100}%</div>
+      <h4>Total Games: {stats.gamesWon + stats.gamesLost}</h4>
     </div>
-  )
+  );
 }
 
 export default App;
